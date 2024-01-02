097e8cf10b184992ff686069f710bd2c973611d1
juanparen15 <jprendon9@gmail.com>
2024-01-02 10:24:42 -0500

    Cambio de logos Alcalde 2024

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-blue elevation-4">
     <!-- Brand Logo -->
     <a href="{{route('home')}}" class="brand-link">
-      <img src="{!!asset('adminlte/dist/img/AdminLTELogo.png')!!}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{!!asset('adminlte/dist/img/AdminLTELogo.jpg')!!}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Plan de Adquisiciones</span>
     </a>
 
