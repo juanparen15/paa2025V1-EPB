1c3f0c2abda70056f8a6795f9ccf3f04febd856c
juanparen15 <jprendon9@gmail.com>
2024-01-04 09:05:50 -0500

    Cambio de fondo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Plan de adquisiciones')
 
 @section('content')
-<div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(homeland/images/hero_bg_2.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+<div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(homeland/images/hero_bg_2024.jpg); height: 940px;" data-aos="fade" data-stellar-background-ratio="0.5">
     <div class="container">
       <div class="row align-items-center justify-content-center text-center">
         <div class="col-md-10">
--- storage/framework/views/4f5f6a359375ba9f59e097a641210eb812e560bb.php
+++ storage/framework/views/4f5f6a359375ba9f59e097a641210eb812e560bb.php
@@ -1,7 +1,7 @@
 <?php $__env->startSection('title', 'Plan de adquisiciones'); ?>
 
 <?php $__env->startSection('content'); ?>
-<div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(homeland/images/hero_bg_2.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+<div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(homeland/images/hero_bg_2024.jpg); height: 940px;" data-aos="fade" data-stellar-background-ratio="0.5">
     <div class="container">
       <div class="row align-items-center justify-content-center text-center">
         <div class="col-md-10">
